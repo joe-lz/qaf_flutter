a3be0c435d4bfbf98999207c03df7addfdd7f2d1
bangdong.chen <bangdong.chen@crgrcent.com>
2020-08-25 09:29:43 +0800

    feat:  theme\navigator

---
+++ lib/screens/home_detail/index.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class HomeDetail extends StatefulWidget {
+  @override
+  _HomeDetailState createState() => _HomeDetailState();
+}
+
+class _HomeDetailState extends State<HomeDetail> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('home detail'),
+    );
+  }
+}
