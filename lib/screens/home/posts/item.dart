5474d1533c3452f7f6f4c26cf2d0a6891becdc50
bangdong.chen <bangdong.chen@crgrcent.com>
2020-09-26 20:30:12 +0800

    fixed:  截屏

--- lib/screens/home/posts/item.dart
+++ lib/screens/home/posts/item.dart
@@ -1,8 +1,11 @@
 import 'dart:async';
+import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:shimmer/shimmer.dart';
 import 'package:lottie/lottie.dart';
+import 'package:screenshot/screenshot.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 
 import 'package:qaf_flutter/provider/theme/dimens.dart';
 import 'package:qaf_flutter/utils/screen_utils.dart';
@@ -23,6 +26,9 @@ class _PostItemComponentState extends State<PostItemComponent> with TickerProvid
   AnimationController _animationController;
   bool _showAnimation = false;
 
+  ScreenshotController _screenshotController = ScreenshotController();
+  File _imageFile;
+
   @override
   void initState() {
     super.initState();
@@ -42,167 +48,197 @@ class _PostItemComponentState extends State<PostItemComponent> with TickerProvid
     super.dispose();
   }
 
+  void handleScreenShot() {
+    _screenshotController.capture(pixelRatio: 2).then((File image) {
+      //Capture Done
+      print(image);
+      setState(() {
+        _imageFile = image;
+      });
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      // padding: EdgeInsets.all(Dimens.gap_dp16),
-      // padding: EdgeInsets.only(top: Dimens.gap_dp16),
-      padding: EdgeInsets.fromLTRB(Dimens.gap_dp16, Dimens.gap_dp16 / 6, Dimens.gap_dp16, Dimens.gap_dp16 / 2),
-      child: Container(
-        width: ScreenUtils.screenW(context) - Dimens.gap_dp16 / 2,
-        // width: ScreenUtils.screenW(context),
-        decoration: BoxDecoration(
-          color: Theme.of(context).canvasColor,
-          borderRadius: BorderRadius.all(Radius.circular(Dimens.radius_15)),
-          // borderRadius: BorderRadius.only(
-          //   topRight: Radius.circular(40),
-          //   topLeft: Radius.circular(40),
-          // ),
-          // boxShadow: [
-          //   BoxShadow(
-          //     color: Colors.grey.withOpacity(0.1),
-          //     spreadRadius: 1,
-          //     blurRadius: 1,
-          //     offset: Offset(0, 0),
-          //   ),
-          // ],
-          // border: Border(
-          //   bottom: BorderSide(width: 0.5, color: Theme.of(context).dividerTheme.color),
-          // ),
-        ),
-        child: Padding(
-          padding: EdgeInsets.all(Dimens.gap_dp16 / 2),
-          child: Column(
-            children: [
-              Container(
-                height: 35,
-                child: Row(
-                  children: [
-                    ClipOval(
-                      child: Container(
-                        width: 32,
-                        height: 32,
-                        color: Theme.of(context).scaffoldBackgroundColor,
-                        child: Image.network(
-                          widget.imageUrl,
-                          fit: BoxFit.cover,
+    return Screenshot(
+      controller: _screenshotController,
+      child: Padding(
+        // padding: EdgeInsets.all(Dimens.gap_dp16),
+        // padding: EdgeInsets.only(top: Dimens.gap_dp16),
+        padding: EdgeInsets.fromLTRB(Dimens.gap_dp16, Dimens.gap_dp16 / 6, Dimens.gap_dp16, Dimens.gap_dp16 / 2),
+        child: Container(
+          width: ScreenUtils.screenW(context) - Dimens.gap_dp16 / 2,
+          // width: ScreenUtils.screenW(context),
+          decoration: BoxDecoration(
+            color: Theme.of(context).canvasColor,
+            borderRadius: BorderRadius.all(Radius.circular(Dimens.radius_15)),
+            // borderRadius: BorderRadius.only(
+            //   topRight: Radius.circular(40),
+            //   topLeft: Radius.circular(40),
+            // ),
+            // boxShadow: [
+            //   BoxShadow(
+            //     color: Colors.grey.withOpacity(0.1),
+            //     spreadRadius: 1,
+            //     blurRadius: 1,
+            //     offset: Offset(0, 0),
+            //   ),
+            // ],
+            // border: Border(
+            //   bottom: BorderSide(width: 0.5, color: Theme.of(context).dividerTheme.color),
+            // ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(Dimens.gap_dp16 / 2),
+            child: Column(
+              children: [
+                Container(
+                  height: 35,
+                  child: Row(
+                    children: [
+                      GestureDetector(
+                        onTap: () {
+                          Navigator.pushNamed(context, "/home_detail");
+                        },
+                        child: ClipOval(
+                          child: Container(
+                            width: 32,
+                            height: 32,
+                            color: Theme.of(context).scaffoldBackgroundColor,
+                            child: Image.network(
+                              widget.imageUrl,
+                              fit: BoxFit.cover,
+                            ),
+                          ),
                         ),
                       ),
-                    ),
-                    Expanded(
-                      flex: 1,
-                      child: Padding(
-                        padding: EdgeInsets.only(left: Dimens.font_sp16 / 2),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Text(
-                              'Dominic Brown',
-                              style: TextStyle(
-                                fontSize: Theme.of(context).textTheme.bodyText1.fontSize,
-                                fontWeight: FontWeight.w600,
-                              ),
+                      Expanded(
+                        flex: 1,
+                        child: GestureDetector(
+                          onTap: () {
+                            Navigator.pushNamed(context, "/home_detail");
+                          },
+                          child: Padding(
+                            padding: EdgeInsets.only(left: Dimens.font_sp16 / 2),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Dominic Brown',
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context).textTheme.bodyText1.fontSize,
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                ),
+                                Text(
+                                  '4分钟之前',
+                                  style: TextStyle(
+                                    fontSize: Theme.of(context).textTheme.caption.fontSize,
+                                    color: Theme.of(context).textTheme.caption.color,
+                                    fontWeight: FontWeight.w500,
+                                  ),
+                                ),
+                              ],
                             ),
-                            Text(
-                              '4分钟之前',
-                              style: TextStyle(
-                                fontSize: Theme.of(context).textTheme.caption.fontSize,
-                                color: Theme.of(context).textTheme.caption.color,
-                                fontWeight: FontWeight.w500,
-                              ),
-                            ),
-                          ],
+                          ),
                         ),
                       ),
-                    ),
-                    Icon(
-                      Icons.more_horiz,
-                      size: 24,
-                    )
-                  ],
+                      Icon(
+                        Icons.more_horiz,
+                        size: 24,
+                      )
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: Dimens.gap_dp16 / 2),
-                child: Stack(
-                  children: [
-                    Row(
-                      children: [
-                        Expanded(
-                          flex: 1,
-                          child: GestureDetector(
-                            onDoubleTap: () {
-                              setState(() {
-                                _showAnimation = true;
-                              });
-                            },
-                            child: ClipRRect(
-                              borderRadius: BorderRadius.all(Radius.circular(Dimens.radius_10)),
-                              child: Container(
-                                height: 300,
-                                color: Theme.of(context).scaffoldBackgroundColor,
-                                child: Image.network(
-                                  widget.imageUrl,
-                                  fit: BoxFit.cover,
+                Padding(
+                  padding: EdgeInsets.only(top: Dimens.gap_dp16 / 2),
+                  child: Stack(
+                    children: [
+                      Row(
+                        children: [
+                          Expanded(
+                            flex: 1,
+                            child: GestureDetector(
+                              onDoubleTap: () {
+                                setState(() {
+                                  _showAnimation = true;
+                                });
+                              },
+                              onTap: () {
+                                Navigator.pushNamed(context, "/home_detail");
+                              },
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.all(Radius.circular(Dimens.radius_10)),
+                                child: Container(
+                                  height: 300,
+                                  color: Theme.of(context).scaffoldBackgroundColor,
+                                  child: Image.network(
+                                    widget.imageUrl,
+                                    fit: BoxFit.cover,
+                                  ),
                                 ),
                               ),
                             ),
                           ),
-                        ),
-                      ],
-                    ),
-                    Visibility(
-                      visible: _showAnimation,
-                      child: Positioned(
-                        left: 0,
-                        top: 0,
-                        child: Container(
-                          width: ScreenUtils.screenW(context) - Dimens.gap_dp16 * 2 - Dimens.gap_dp16,
-                          height: 300,
-                          child: Center(
-                            child: Lottie.asset(
-                              'assets/lottie/like.json',
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.fill,
-                              animate: true,
-                              repeat: false,
-                              controller: _animationController,
-                              onLoaded: (composition) {
-                                _animationController
-                                  ..duration = composition.duration
-                                  ..reset()
-                                  ..forward();
-                              },
+                        ],
+                      ),
+                      Visibility(
+                        visible: _showAnimation,
+                        child: Positioned(
+                          left: 0,
+                          top: 0,
+                          child: Container(
+                            width: ScreenUtils.screenW(context) - Dimens.gap_dp16 * 2 - Dimens.gap_dp16,
+                            height: 300,
+                            child: Center(
+                              child: Lottie.asset(
+                                'assets/lottie/like.json',
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.fill,
+                                animate: true,
+                                repeat: false,
+                                controller: _animationController,
+                                onLoaded: (composition) {
+                                  _animationController
+                                    ..duration = composition.duration
+                                    ..reset()
+                                    ..forward();
+                                },
+                              ),
                             ),
                           ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
-              ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(
-                  Dimens.gap_dp16 / 2,
-                  Dimens.gap_dp16,
-                  Dimens.gap_dp16 / 2,
-                  Dimens.gap_dp16,
+                Padding(
+                  padding: EdgeInsets.fromLTRB(
+                    Dimens.gap_dp16 / 2,
+                    Dimens.gap_dp16,
+                    Dimens.gap_dp16 / 2,
+                    Dimens.gap_dp16,
+                  ),
+                  child: Row(
+                    children: [
+                      IconFont(IconNames.heart, size: 24),
+                      SizedBox(width: 20),
+                      IconFont(IconNames.xiaoxi, size: 18),
+                      SizedBox(width: 20),
+                      GestureDetector(
+                        child: IconFont(IconNames.send_s, size: 24),
+                        onTap: handleScreenShot,
+                      ),
+                      Spacer(),
+                      IconFont(IconNames.shoucang, size: 18),
+                    ],
+                  ),
                 ),
-                child: Row(
-                  children: [
-                    // Icon(Icons.share, size: 24),
-                    IconFont(IconNames.heart, size: 24),
-                    SizedBox(width: 20),
-                    IconFont(IconNames.xiaoxi, size: 18),
-                    SizedBox(width: 20),
-                    IconFont(IconNames.send_s, size: 24),
-                    Spacer(),
-                    IconFont(IconNames.shoucang, size: 18),
-                  ],
-                ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
--- lib/screens/home_detail/index.dart
+++ lib/screens/home_detail/index.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:qaf_flutter/utils/screen_utils.dart';
 
 class HomeDetailScreen extends StatefulWidget {
   @override
@@ -9,7 +10,19 @@ class _HomeDetailScreenState extends State<HomeDetailScreen> {
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text('home detail'),
+      color: Colors.white,
+      child: Column(
+        children: [
+          Container(
+            width: ScreenUtils.screenW(context),
+            height: 300,
+            child: Image.network(
+              'https://images.pexels.com/photos/4887433/pexels-photo-4887433.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+              fit: BoxFit.cover,
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
